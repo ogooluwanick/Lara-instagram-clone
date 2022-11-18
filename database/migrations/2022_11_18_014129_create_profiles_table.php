fafd6522f50fb480bbfc6797f8683c76fe8f723e
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-18 03:00:43 +0100

    profile models, migrations and table

---
+++ database/migrations/2022_11_18_014129_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");                      #fk_user table
+            $table->string("title")->nullable();
+            $table->text("desc")->nullable();
+            $table->string("url")->nullable();
+            $table->timestamps();
+
+            $table->index("user_id");                                                #index for fk_user table
+        });     
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
