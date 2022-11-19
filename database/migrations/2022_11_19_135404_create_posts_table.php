e765b4199e578f6424603a44a671176bfe597d7e
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-19 15:11:13 +0100

    posts relationships

---
+++ database/migrations/2022_11_19_135404_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");                      #fk_user table
+            $table->timestamps();
+            $table->string("caption")->nullable();
+            $table->string("image");
+            
+            $table->index("user_id");                                                #index for fk_user table
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
